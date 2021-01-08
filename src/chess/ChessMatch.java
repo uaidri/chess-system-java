46a230b96e5abaaf6484d56de7bfcfdaeb602693
uaidri <uaidri@gmail.com>
2021-01-08 11:07:50 -0300

    Bishop

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
@@ -208,10 +209,11 @@ public class ChessMatch {
 		/*
 		 * placeNewPiece('b', 1, new Knight(board, Color.WHITE)); placeNewPiece('c', 1,
 		 * new Bishop(board, Color.WHITE)); placeNewPiece('d', 1, new Queen(board,
-		 * Color.WHITE)); placeNewPiece('e', 1, new King(board, Color.WHITE, this));
-		 * placeNewPiece('f', 1, new Bishop(board, Color.WHITE)); placeNewPiece('g', 1,
-		 * new Knight(board, Color.WHITE));
-		 */
+		 * Color.WHITE)); 
+		 * placeNewPiece('e', 1, new King(board, Color.WHITE, this));*/
+		  placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+		 // placeNewPiece('g', 1,new Knight(board, Color.WHITE));
+		 
 		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('a', 2, new King(board, Color.WHITE));
 		placeNewPiece('b', 2, new Pawn(board, Color.WHITE, this));
@@ -226,10 +228,10 @@ public class ChessMatch {
 		/*
 		 * placeNewPiece('b', 8, new Knight(board, Color.BLACK)); placeNewPiece('c', 8,
 		 * new Bishop(board, Color.BLACK)); placeNewPiece('d', 8, new Queen(board,
-		 * Color.BLACK)); placeNewPiece('e', 8, new King(board, Color.BLACK, this));
-		 * placeNewPiece('f', 8, new Bishop(board, Color.BLACK)); placeNewPiece('g', 8,
-		 * new Knight(board, Color.BLACK));
-		 */
+		 * Color.BLACK)); placeNewPiece('e', 8, new King(board, Color.BLACK, this));*/
+		 placeNewPiece('f', 8, new Bishop(board, Color.BLACK)); 
+		// placeNewPiece('g', 8, new Knight(board, Color.BLACK));
+		 
 		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('a', 7, new King(board, Color.BLACK));
 		placeNewPiece('b', 7, new Pawn(board, Color.BLACK, this));
